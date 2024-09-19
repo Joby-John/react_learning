b3eb3de58da4b01867db728fe2d30c6223314a28
Joby-John <joby432john@gmail.com>
2024-09-19 13:14:26 +0530

    starting with react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header.jsx"
+import Footer from "./footer.jsx";
+import Food from "./food.jsx";
+
+function App() {
+  
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Food/>
+    <Footer/>
+    </>
+    
+  );
+}
+
+export default App
